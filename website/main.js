d50ca870c97ad96aba070bb2665928c3fd778d52
Mark Arts <105424@live.nl>
2013-10-09 23:04:31 +0200

    Added some aditional examples to the demo

--- website/main.js
+++ website/main.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
-	random(20);
+	getGroups(5);
+	getPlussers(5);
+	getGroupFromId(614);
+	getPlusserFromId(1);
 
-	$('#button').click(function(){
-		random($('#amount').val());
+	$('#buttonG').click(function(){
+		getGroups($('#amountG').val());
+	});
+
+	$('#buttonP').click(function(){
+		getPlussers($('#amountP').val());
+	});
+
+	$('#buttonF').click(function(){
+		getGroupFromId($('#amountF').val());
+	});	
+	$('#buttonH').click(function(){
+		getPlusserFromId($('#amountH').val());
 	});
 
 });
 
-function random(amount){
+function getGroups(amount){
 	var html = $("#groups");
 
 	html.fadeOut(function(){
@@ -22,6 +36,71 @@ function random(amount){
 
 				html.append("<div class='group'>"+group.id+" : "+group.name+"</div>");
 				i++;
+			});
+		});
+	});
+	html.fadeIn();
+}
+
+function getPlussers(amount){
+	var html = $("#plussers");
+
+	html.fadeOut(function(){
+		html.empty();
+		$.get( "http://www.thomassio.nl:2001/plussers").done(function(data){
+			var i = 0;
+			$.each(data,function(key,plusser){
+
+				if(i >= amount){
+					return
+				}
+
+				html.append("<div class='plusser'>"+plusser.id+" : "+plusser.lastName+" : "+plusser.firstName+"</div>");
+				i++;
+			});
+		});
+	});
+	html.fadeIn();
+}
+
+function getGroupFromId(id){
+	var html = $("#groupF");
+
+	html.fadeOut(function(){
+		html.empty();
+		$.get( "http://www.thomassio.nl:2001/groups/"+id).done(function(data){
+
+			html.append("<div class='plus'>"+data.id+" : "+data.name+"</div>");
+
+			html.find(".plus").append("<ul></ul>");
+
+			$.each(data.plussers, function(key,value){
+				console.log(value);
+				html.find(".plus").find("ul").append(
+					"<li>"+key+
+					" id: "+value+"</li>"
+				);
+			});
+		});
+	});
+	html.fadeIn();
+}
+function getPlusserFromId(id){
+	var html = $("#plusserH");
+
+	html.fadeOut(function(){
+		html.empty();
+		$.get( "http://www.thomassio.nl:2001/plussers/"+id).done(function(data){
+			html.append("<div class='plus'>"+data.id+" : "+data.firstName+"</div>");
+			console.log(data);
+			html.find(".plus").append("<ul></ul>");
+
+			$.each(data.groups, function(key,value){
+				console.log(value);
+				html.find(".plus").find("ul").append(
+					"<li>"+key+
+					" id: "+value+"</li>"
+				);
 			});
 		});
 	});
