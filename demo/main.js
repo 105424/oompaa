6e087a8aee8fee602576c42c378197e7590fe300
Mark Arts <105424@live.nl>
2013-10-28 19:52:01 +0100

    Added login demo, Added base

--- demo/main.js
+++ demo/main.js
@@ -1,185 +1,62 @@
-var link = "www.thomassio.nl:2001";
+var link = "thomassio.nl:2001";
+var firstPage = "all";
 //var link = "localhost:2001";
+
+//console.log = function(){}; //Turns of console.log();
 
 $(document).ready(function(){
 
-	$('h1').html(link);
+	include("header/header", "body");
+	
+	include("menu", "body");
+	
+	include("pages/"+firstPage,"body");
 
-	var container = $("#container")
-	container.masonry( {
-	  itemSelector: '.demo'
-	});
-
-	getGroups(5);
-	getPlussers(5);
-	getGroupFromId(614);
-	getPlusserFromId(1);
-
-
-	$('#linkB').click(function(){
-		link = $('#linkV').val();
-		getGroups(5);
-		getPlussers(5);
-		getGroupFromId(614);
-		getPlusserFromId(1);		
-		$('h1').html(link);
-	});
-
-	$('#buttonG').click(function(){
-		getGroups($('#amountG').val());
-	});
-
-	$('#buttonP').click(function(){
-		getPlussers($('#amountP').val());
-	});
-
-	$('#buttonF').click(function(){
-		getGroupFromId($('#amountF').val());
-	});	
-	$('#buttonH').click(function(){
-		getPlusserFromId($('#amountH').val());
-	});	
-	$('#buttonI').click(function(){
-		addPlusserToGRoup($('#amountIa').val(),$('#amountIb').val())
-	});
-	$('#buttonJ').click(function(){
-		customGet($('#amountJ').val());
-	});
+	include("footer/footer", "body");	
 });
 
+function navToPage(page){
+/*	$('.page').fadeOut(function(){
+		$('.page').remove();
 
-function getGroups(amount){
-	var html = $("#groups");
-	var html2 = "";
+		include("pages/"+page, "body");
 
-	html.fadeOut(function(){
-		//html.empty();
-		$.get( "http://"+link+"/groups").done(function(data){
-				console.log(data);
-			var i = 0;
-			$.each(data,function(key,group){
+		$('.page').css("display","none");
+		$('.page').fadeIn('slow');
+	});*/
 
-				if(i >= amount){
-					return
-				}
+	$('.page').animate({
+			opacity: 0.25,
+    	left: "+=5000",
+   		height: "toggle"
+		},1000,function(){
+			$('.page').remove();
 
-				html2 += "<div class='group'>"+group.id+" : "+group.name+"</div>";
-				i++;
-			});
-			html.html(html2);
-			html.fadeIn();
-			$("#container").masonry();
-		});
+			include("pages/"+page, "body");
+			
+			$('.page').css("display","none");
+			$('.page').fadeIn('slow');
 	});
 }
 
-function getPlussers(amount){
-	var html = $("#plussers");
-	var html2 = "";
+function include(file,inTo,shouldReturn,async){
+	var answer;
+	var asyn = false;
 
-	html.fadeOut(function(){
-		$.get( "http://"+link+"/plussers").done(function(data){
-				console.log(data);
-			var i = 0;
-			$.each(data,function(key,plusser){
+	if(async != undefined) var asyn = async;
 
-				if(i >= amount){
-					return
-				}
-
-				html2 += ("<div class='plusser'>"+plusser.id+" : "+plusser.lastName+" : "+plusser.firstName+"</div>");
-				i++;
-			});
-			html.html(html2);
-			html.fadeIn();
-			$("#container").masonry();
-		});
+	$.ajax({
+	     async: asyn,
+	     type: 'GET',
+	     url: file + '.html', // Nodig omdat somigee servers geen bestanden vinden zonder extensie
+	     success: function(data) {
+	        if(shouldReturn){
+	        	answer = data;
+		      }
+					else{
+	        	$(inTo).append(data);
+					}
+	     }
 	});
-}
-
-function getGroupFromId(id){
-	var html = $("#groupF");
-	var html2 = $("<div class='plus'>");;
-	html.fadeOut(function(){
-		html.empty();
-		$.get( "http://"+link+"/groups/"+id).done(function(data){
-			console.log(data);
-			html2.append(data.id+" : "+data.name);
-
-			html2.append("<ul></ul>");
-
-			$.each(data.plussers, function(key,value){
-				html2.find("ul").append(
-					"<li>"+key+
-					" id: "+value+"</li>"
-				);
-			});
-			html.html(html2.html());
-			html.fadeIn();
-			$("#container").masonry();
-		});
-	});
-}
-function getPlusserFromId(id){
-	var html = $("#plusserH");
-
-	html.fadeOut(function(){
-		html.empty();
-		$.get( "http://"+link+"/plussers/"+id).done(function(data){
-				console.log(data);
-			html.append("<div class='plus'>"+data.id+" : "+data.firstName+"</div>");
-			console.log(data);
-			html.find(".plus").append("<ul></ul>");
-
-			$.each(data.groups, function(key,value){
-				html.find(".plus").find("ul").append(
-					"<li>"+key+
-					" id: "+value+"</li>"
-				);
-			});
-			html.fadeIn();
-			$("#container").masonry();
-		});
-	});
-}
-
-function addPlusserToGRoup(groupId,plusId){
-	console.log(plusId);
-	var html = $("#groupI");
-	$.post("http://"+link+"/groups/"+groupId,{"plussers":plusId}).done(function(data){
-		html.html(JSON.stringify(data));
-					$("#container").masonry();
-	}).error(function(data){
-		html.html(JSON.stringify(data));
-					$("#container").masonry();
-	});
-
-}
-function customGet(get){
-	var html = $("#groupJ");
-	var html2 = $("<div class='plus'>");;
-	console.log(1);
-	html.fadeOut(function(){
-		html.empty();
-		console.log("http://"+link+"/"+get);
-		$.get( "http://"+link+"/"+get).done(function(data){
-			console.log(data);
-			html2.append(JSON.stringify(data));
-
-			//html2.append("<ul></ul>");
-
-			html.html(html2.html());
-			html.fadeIn();
-			$("#container").masonry();
-		}).error(function(err){
-			console.log(err);
-			html2.append(err.responseText);
-
-			//html2.append("<ul></ul>");
-
-			html.html(html2.html());
-			html.fadeIn();
-			$("#container").masonry();
-		});
-	});
+	if(shouldReturn) return answer;
 }
--- website/main.js
+++ website/main.js
@@ -1,42 +1,52 @@
-var link = "thomassio.nl:2001";
-var firstPage = "all";
-//var link = "localhost:2001";
+// globals (Allways uppercase)
+var SERVER = "thomassio.nl:2001";
+//var SERVER = "localhost:2001";
+var FIRSTPAGE = "home";
+var PLUSSER;
 
 //console.log = function(){}; //Turns of console.log();
 
 $(document).ready(function(){
 
 	include("header/header", "body");
+
+	$('body').append("<div id='content'></div>")
 	
-	include("menu", "body");
+	include("blocks/menu", "#content");
 	
-	include("pages/"+firstPage,"body");
+	include("pages/"+FIRSTPAGE,"#content");
 
 	include("footer/footer", "body");	
+
 });
 
 function navToPage(page){
-/*	$('.page').fadeOut(function(){
+	if(PLUSSER > 0){
+		if(page == "home")
+			page = "profile";
+	}
+
+	$('.page').fadeOut(function(){
 		$('.page').remove();
 
-		include("pages/"+page, "body");
+		include("pages/"+page, "#content");
 
 		$('.page').css("display","none");
 		$('.page').fadeIn('slow');
-	});*/
+	});
 
-	$('.page').animate({
+/*	$('.page').animate({
 			opacity: 0.25,
     	left: "+=5000",
    		height: "toggle"
 		},1000,function(){
 			$('.page').remove();
 
-			include("pages/"+page, "body");
+			include("pages/"+page, "#content");
 			
 			$('.page').css("display","none");
 			$('.page').fadeIn('slow');
-	});
+	});*/
 }
 
 function include(file,inTo,shouldReturn,async){
@@ -46,17 +56,20 @@ function include(file,inTo,shouldReturn,async){
 	if(async != undefined) var asyn = async;
 
 	$.ajax({
-	     async: asyn,
-	     type: 'GET',
-	     url: file + '.html', // Nodig omdat somigee servers geen bestanden vinden zonder extensie
-	     success: function(data) {
-	        if(shouldReturn){
-	        	answer = data;
-		      }
-					else{
-	        	$(inTo).append(data);
-					}
-	     }
+		async: asyn,
+		type: 'GET',
+		url: file + '.html', // Nodig omdat somigee servers geen bestanden vinden zonder extensie
+		success: function(data) {
+		  if(shouldReturn){
+		  	answer = data;
+		  }
+			else{
+		  	$(inTo).append(data);
+			}
+		},
+		error: function(err){
+			return err;
+		}
 	});
 	if(shouldReturn) return answer;
 }
